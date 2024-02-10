c76b8141b3164c92e2faf81cd2d7b42538215675
Philip Müller <philip-mueller@mailbox.org>
2024-02-10 14:15:17 +0100

    fix: adapt gpt-3.5-turbo maxContextLength to context sizes in the official docs

--- lib/src/main/java/com/knuddels/jtokkit/api/ModelType.java
+++ lib/src/main/java/com/knuddels/jtokkit/api/ModelType.java
@@ -10,8 +10,8 @@ public enum ModelType {
     // chat
     GPT_4("gpt-4", EncodingType.CL100K_BASE, 8192),
     GPT_4_32K("gpt-4-32k", EncodingType.CL100K_BASE, 32768),
-    GPT_3_5_TURBO("gpt-3.5-turbo", EncodingType.CL100K_BASE, 4097),
-    GPT_3_5_TURBO_16K("gpt-3.5-turbo-16k", EncodingType.CL100K_BASE, 16384),
+    GPT_3_5_TURBO("gpt-3.5-turbo", EncodingType.CL100K_BASE, 4096),
+    GPT_3_5_TURBO_16K("gpt-3.5-turbo-16k", EncodingType.CL100K_BASE, 16385),
 
     // text
     TEXT_DAVINCI_003("text-davinci-003", EncodingType.P50K_BASE, 4097),
